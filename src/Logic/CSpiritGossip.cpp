2c9e80b5451b0d7c01f46979484a9c995d9f1b55
RyanLin <82807618+RyanLin0122@users.noreply.github.com>
2026-03-24 13:01:16 +0800

    Revert speculative spirit ID and restore 3-condition bounds

--- src/Logic/CSpiritGossip.cpp
+++ src/Logic/CSpiritGossip.cpp
@@ -38,9 +38,6 @@ char* CSpiritGossip::CheckSpiritGossip()
         return nullptr;
 
     const stGossipInfo& info = it->second;
-    if (info.count == 0)
-        return nullptr;
-
     int textIdx = std::rand() % info.count;
     char* text = g_DCTTextManager.GetText(info.textIds[textIdx]);
     return text;
